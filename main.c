0136b455c3795dd291d74e2c040887a894776e40
Hamid-1430 <s185229@student.dtu.dk>
2025-09-10 16:27:25 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define PRICE_COUNT_MAX 10
+
+int product_prices[] = {13, 140, 900, 2};
+int main(void) {
+  for (int i = 0; i =0;< PRICE_COUNT_MAX; ++i)
+  printf(format:"%d: %3d DDK\n",i,product_prices[i]);
+    // YOUR CODE HERE
+}
