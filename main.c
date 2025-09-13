064498a203991cfbddcd09676ee8d69a6078aaa8
Hamid-1430 <s185229@student.dtu.dk>
2025-09-14 01:39:33 +0200

    Intial commit: Convey program

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-#define PRICE_COUNT_MAX 10
 
-int product_prices[] = {13, 140, 900, 2};
-int main(void) {
-  for (int i = 0; i =0;< PRICE_COUNT_MAX; ++i)
-  printf(format:"%d: %3d DDK\n",i,product_prices[i]);
-    // YOUR CODE HERE
+const double package_Weight_Per_Motor = 5.6;
+
+int main(void)
+{
+    int motorCount;
+    double packageWeight;
+
+    printf("How many motors are carrying the packages? ");
+    fflush(stdout);
+    if (scanf("%d", &motorCount)!= 1) return 0;
+
+    printf("How many kg of packages do we expect? ");
+    fflush(stdout);
+    if (scanf("%lf", &packageWeight)!= 1) return 0;
+
+    // Each motor can take up to 5.6 kg on avarage.
+
+    if (motorCount > 0 && packageWeight / motorCount <= package_Weight_Per_Motor) {
+        printf("Yes! The conveyor belt can carry the packages.\n");
+    } else {
+        printf("No. The conveyor belt cannot carry the packages.\n");
+    }
+
+    return 0;
 }
